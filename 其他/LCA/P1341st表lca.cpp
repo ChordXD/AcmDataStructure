aa76da76ddcbe38ebcce4807a98c67079b3bfb31
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2019-08-28 20:42:17 +0800

    Update P1341st表lca.cpp

--- 其他/LCA/P1341st表lca.cpp
+++ 其他/LCA/P1341st表lca.cpp
@@ -1,80 +1,94 @@
-#include<iostream>
+// luogu-judger-enable-o2
 #include<cstdio>
 #include<cstring>
-#include<queue>
-#include <stack>
 #include <vector>
 #include <algorithm>
 using namespace std;
-const int maxn = 100;
-int n;
-bool mapp[maxn][maxn];
-int cnt[maxn];
-bool vis[maxn][maxn];
-stack<int> res;
-int siz = 53;
-
-int convert1(char c){
-    if (c >= 'A' && c <= 'Z')
-        return (int)c - 'A';
-    return (int)c - 'a' + 26;
-}
-char convert2(int x){
-    if (x >= 0 && x <= 25)
-        return (char)x + 'A';
-    return (char)(x - 26) + 'a';
+inline int read() {
+    char ch = getchar(); int x = 0, f = 1;
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') f = -1;
+        ch = getchar();
+    } while('0' <= ch && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    } return x * f;
 }
 
-void euler(int u){
-    for(int v = 0; v < siz; v++){
-        if(mapp[u][v] && !vis[u][v]){
-            vis[u][v] = vis[v][u] = true;
-            euler(v);
-//            cout << "yes" << endl;
-            res.push(v);
+const int maxn = 5e5 + 10;
+const int maxbit = 20;
+vector<int> G[maxn];
+int deep[maxn<<1];
+int order[maxn<<1];
+int st[maxn<<1][maxbit];
+int first_list[maxn];
+int n, m, s;
+int lg[maxn<<1];
+int father[maxn][maxbit];
+
+int cnt = 0;
+void dfs(int nowp, int dep){
+    ++cnt;
+    first_list[nowp] = cnt;
+    order[cnt] = nowp;
+    deep[cnt] = dep+1;
+
+    for(int i = 0; i < G[nowp].size(); i++){
+        int to = G[nowp][i];
+        if(first_list[to] == 0){
+            dfs(to, dep+1);
+            ++cnt;
+            order[cnt] = nowp;
+            deep[cnt] = dep + 1;
+        }
+    }
+}
+
+void st_init(){
+    for(int i = 1; i <= cnt; i++){
+        st[i][0] = i;
+    }
+    int a, b;
+    for(int j = 1; j <= lg[cnt]; j++){
+        for(int i = 1; i+(1<<j)-1 <= cnt; i++){
+            a = st[i][j-1];
+            b = st[i+(1<<j-1)][j-1];
+            if(deep[a] < deep[b]){
+                st[i][j] = a;
+            }else{
+                st[i][j] = b;
+            }
         }
     }
 }
 
 int main(){
-    scanf("%d", &n);
-    for(int i = 1; i <= n; i++){
-        char a, b;
-        cin >> a >> b;
-        int x = convert1(a), y = convert1(b);
-        mapp[x][y] = mapp[y][x] = true;
-        cnt[x]++;
-        cnt[y]++;
+    lg[0] = -1;
+    for(int i = 1; i <= maxn*2; i++){
+        lg[i] = lg[i>>1] + 1;
     }
-    int total = 0;//入度奇数节点
-    int min1 = 1e6, min2 = 1e6;
-    for(int i = 0; i < siz; i++){
-        if(cnt[i]%2 == 1){
-            total++;
-            min1 = min(i, min1);
-        }else if(cnt[i] > 0){
-            min2 = min(i, min2);
-        }
+    n=read(); m=read(); s=read();
+    int x, y;
+    for(int i = 1; i <= n - 1; i++){
+        x=read(); y=read();
+        G[x].push_back(y);
+        G[y].push_back(x);
     }
-
-    if(total!=2 && total!=0){
-        printf("No Solution\n");
-    }else{
-        int fn;
-        if(total==0){
-            fn = min2;
+    dfs(s, 0);
+    st_init();
+    while (m--) {
+        x=read(); y=read();
+        x = first_list[x];
+        y = first_list[y];
+        if(x > y) swap(x, y);
+        int k = lg[y-x];
+        int a, b;
+        a = st[x][k];
+        b = st[y-(1<<k)+1][k];
+        if(deep[a] < deep[b]){
+            printf("%d\n",order[a]);
         }else{
-            fn = min1;
+            printf("%d\n",order[b]);
         }
-        euler(fn);
-        res.push(fn);
-        while (!res.empty()) {
-            int itop = res.top();
-            res.pop();
-            char cc = convert2(itop);
-            printf("%c",cc);
-        }
-        printf("\n");
     }
-    return 0;
 }
